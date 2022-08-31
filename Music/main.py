ff6dd36da6d741c204746c4a92fb0ececf75de0f
Siddhu M <61168189+RedRangerr@users.noreply.github.com>
2022-08-31 14:59:49 -0500

    needs more work

--- Music/main.py
+++ Music/main.py
@@ -6,9 +6,11 @@ import frequencies
 import util
 import time
 
-def crazy_train():
-    BPM = 140
+BPM = 130
 
+def crazy_train():
+
+    # #1st line
     for i in range(2):
         sine(calc_octative(F_SHARP_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
         sine(calc_octative(F_SHARP_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
@@ -30,7 +32,7 @@ def crazy_train():
         time.sleep(calc_note_length(1, BPM))
 
 
-    
+    #second line
     for i in range(3):
         #1st bar    
         sine(calc_octative(F_SHARP_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
@@ -64,14 +66,41 @@ def crazy_train():
     sine(calc_octative(D_FREQUENCY, 5), calc_note_length(2, BPM))
     sine(calc_octative(E_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
 
+    third_line()
+
+
+def third_line():
+    #3rd line
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+
+    sine(calc_octative(A_BASE_FREQUENCY, 5), calc_note_length(0.5, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(G_SHARP_BASE_FREQUENCY, 5), calc_note_length(0.5, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+
+    sine(calc_octative(F_SHARP_BASE_FREQUENCY, 5), calc_note_length(0.5, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.25, BPM))
+    sine(calc_octative(E_BASE_FREQUENCY, 5), calc_note_length(1.5, BPM))
+
+    sine(calc_octative(G_SHARP_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
+    sine(calc_octative(G_SHARP_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
+    sine(calc_octative(A_BASE_FREQUENCY, 4), calc_note_length(0.5, BPM))
+
+
+third_line()
 
 
 
 
-
-
-
-
-
-crazy_train()
 
