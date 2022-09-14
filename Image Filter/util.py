938512136e8fa86970c2165f4bd44e7c147a32a5
smohan67 <siddhu.mohan6@gmail.com>
2022-09-13 22:41:38 -0500

    logic

--- Image Filter/util.py
+++ Image Filter/util.py
@@ -10,7 +10,14 @@ def create_grayscale_arr(min_gray, max_gray):
     max_grayscale = [max_gray, max_gray, max_gray]
     return [np.array(min_grayscale, dtype = "uint8"), np.array(max_grayscale, dtype = "uint8")]
 
-    
+def create_image_part(grayscale_image, min_grayscale, max_grayscale, color):
+    block_all_but_certain_color = cv2.inRange(grayscale_image,min_grayscale,max_grayscale)
+    image_height = grayscale_image.shape[0]
+    image_width = grayscale_image.shape[1]
+    image_channels = grayscale_image.shape[2]
+    paper = np.zeros((image_height,image_width,image_channels), np.uint8)
+    paper[0:image_height,0:image_width, 0:image_channels] = color
+    return cv2.bitwise_or(paper, paper, mask= block_all_but_certain_color)    
 
     
     
