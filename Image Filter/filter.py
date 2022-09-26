bc665f3c4437e1a48515c02702c0f866664a3592
Siddhu M <61168189+RedRangerr@users.noreply.github.com>
2022-09-26 14:06:04 -0500

    add in 6 colors

--- Image Filter/filter.py
+++ Image Filter/filter.py
@@ -5,7 +5,11 @@ import cv2
 import numpy
 import os.path
 from logic import *
+
 #gets the image from the user
+
+
+
 print ("Save your original image in the same folder as this program.")
 filename_valid = False
 while filename_valid == False:
@@ -25,12 +29,13 @@ grayscale_image = cv2.cvtColor(grayscale_image_simple, cv2.COLOR_GRAY2BGR)
 cv2.namedWindow("Trackbar")
 cv2.namedWindow('Customized Image')
 
-#create sliders
+#create sliders for colors
 cv2.createTrackbar("Color1Break", "Trackbar", 0, 255, lambda x:None)
 cv2.createTrackbar("Color2Break", "Trackbar", 0, 255, lambda x:None)
 cv2.createTrackbar("Color3Break", "Trackbar", 0, 255, lambda x:None)
 cv2.createTrackbar("Color4Break", "Trackbar", 0, 255, lambda x:None)
 cv2.createTrackbar("Color5Break", "Trackbar", 0, 255, lambda x:None)
+cv2.createTrackbar("Color6Break", "Trackbar", 0, 255, lambda x:None)
 
 
 
@@ -43,20 +48,24 @@ while keypressed != 27 and keypressed != ord('s'):
     green_parts_of_image = create_image_part(grayscale_image, cv2.getTrackbarPos("Color2Break", "Trackbar")+1, cv2.getTrackbarPos("Color3Break", "Trackbar"), [0,255,0])
     blue_parts_of_image = create_image_part(grayscale_image, cv2.getTrackbarPos("Color3Break", "Trackbar")+1, cv2.getTrackbarPos("Color4Break", "Trackbar"), [255, 0, 0])
     purple_parts_of_image = create_image_part(grayscale_image, cv2.getTrackbarPos("Color4Break", "Trackbar")+1, cv2.getTrackbarPos("Color5Break", "Trackbar"), [250, 230, 30])
+    gold_parts_of_image = create_image_part(grayscale_image,  cv2.getTrackbarPos("Color5Break", "Trackbar")+1, cv2.getTrackbarPos("Color6Break", "Trackbar"), [38,162,193])
 
     customized_image = cv2.bitwise_or(red_parts_of_image, yellow_parts_of_image)
     customized_image = cv2.bitwise_or(customized_image, green_parts_of_image)
     customized_image = cv2.bitwise_or(customized_image, blue_parts_of_image)
     customized_image = cv2.bitwise_or(customized_image, purple_parts_of_image)
-
-
+    customized_image = cv2.bitwise_or(customized_image, gold_parts_of_image)
+    #shows windows
     cv2.imshow('Red Parts of Image',red_parts_of_image)
     cv2.imshow('Yellow Parts of Image',purple_parts_of_image)
     cv2.imshow('Customized Image',customized_image)
 
+    #check for keypress every 30ms
     keypressed = cv2.waitKey(30)
     if keypressed == 27:
         cv2.destroyAllWindows()
     elif keypressed == ord('s'): 
-            cv2.imwrite(input('Save Customized Image as:'),customized_image)
-            cv2.destroyAllWindows()
+        file_name = input('Save Customized Image as:')
+        cv2.imwrite("OutputImages/"+file_name,customized_image)
+        cv2.destroyAllWindows()
+
