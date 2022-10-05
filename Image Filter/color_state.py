214485409befbd574c0e422117b68c52a901f5e1
Siddhu M <61168189+RedRangerr@users.noreply.github.com>
2022-10-05 14:20:23 -0500

    add comments

--- Image Filter/color_state.py
+++ Image Filter/color_state.py
@@ -2,23 +2,34 @@ from re import I
 import cv2
 import logic
 
+#class to manage the states of all the colors
 class ColorManager:
+    #initializes the class with a window name, original image, grey image, a value for the greybreak value, and a dictionary of colors that map ids to a list of rgb values
     def __init__(self, window_name, original_image, grayscale_image, greybreak_val = 0, original_colors = {}) -> None:
-        self.current_color = 1
-        self.colors = original_colors
-        self.window_name = window_name
-        self.original_image = original_image
-        self.grayscale_image = grayscale_image
+        #creates instance variables
+        self.current_color = 1#the color that we are currently editing
+        self.colors = original_colors#all of our colors
+        self.window_name = window_name#our window name
+        self.original_image = original_image#our original image
+        self.grayscale_image = grayscale_image#our grayscale image
+        #create trackbars for editing the bgr values of the color 
+        #the lamda functions trigger updates to the image and color data of the class 
         cv2.createTrackbar("ColorB", window_name, 0, 255, lambda x:self.update_color_trackbars(0, x))
         cv2.createTrackbar("ColorG", window_name, 0, 255, lambda x:self.update_color_trackbars(1, x))
         cv2.createTrackbar("ColorR", window_name, 0, 255, lambda x:self.update_color_trackbars(2, x))
+        #greyscale break trackbar which controls how the different colors are divided withn the image
+        #the lamda updates the grey value
         cv2.createTrackbar("ColorBreak", window_name, 0, 255, lambda x:self.on_trackbar_update())
         cv2.createTrackbar("CurColor", window_name, 1, 10, lambda x:self.on_curent_color_trackbar_update(x))
+        #set the greyscale trackbar equal to the value of 
         if greybreak_val != 0:
             cv2.setTrackbarPos("ColorBreak", window_name, greybreak_val)
+        #function that updates the current color to the 1st bar
         self.set_current_color(1)
+        #updates the image
         self.on_trackbar_update()
 
+    #function that sets the current color and updates the trackbar based on its value
     def set_current_color(self, color_new):
         if (color_new < 1):
             return
@@ -27,16 +38,21 @@ class ColorManager:
         cv2.setTrackbarPos("ColorG", self.window_name, self.colors[color_new][1])
         cv2.setTrackbarPos("ColorR", self.window_name, self.colors[color_new][2])
     
+    #refreshes the final image with the current color and grey break values
     def on_trackbar_update(self):
         logic.refresh_image(self.grayscale_image, cv2.getTrackbarPos("ColorBreak", self.window_name), self.colors)
-        
+    
+    #sets the current color when
     def on_curent_color_trackbar_update(self,x):
         self.set_current_color(x)
     
+    #function is called when any of the color sliders are updated
+    #updates the current color's value with the new value and reloads the image
     def update_color_trackbars(self, id, val):
         self.colors[self.current_color][id] = val
         logic.refresh_image(self.grayscale_image, cv2.getTrackbarPos("ColorBreak", self.window_name), self.colors)
 
+    #refreshes and returns the final image
     def get_customized_image(self):
         return logic.refresh_image(self.grayscale_image, cv2.getTrackbarPos("ColorBreak", self.window_name), self.colors, True)
         
--- Image Filter/filter.py
+++ Image Filter/filter.py
@@ -37,10 +37,13 @@ grayscale_image = cv2.cvtColor(grayscale_image_simple, cv2.COLOR_GRAY2BGR)
 
 #asks the user if they want to load in json data to preset the colors
 json_option = input("Would you like to load colors? (Y/N) ")
+#variable to represent the json file
 json_file = None
+#if the user enters a y, continue loading
 if json_option == "Y" or json_option == "y":
     while True:
         file_name = input("Enter the file name:")
+        #chack if file is valid path
         if not os.path.isfile(file_name):
             try_again_str = input("Couldn't find the file path specified. Do you want to try again?(y/n)")
             if not try_again_str == "y":
@@ -48,7 +51,9 @@ if json_option == "Y" or json_option == "y":
             else:
                 continue
         else:
+            #open file and load in json 
             with open(file_name, 'r') as f:
+                #function to load in json
                 json_file = json.load(f)
                 cv2.destroyAllWindows()
             print("Loaded color combinations!")
@@ -76,21 +81,29 @@ if json_file != None:
     colors_start = data[1]
     print(colors_start)
     
-#creates a new instance of the color manager class
+#creates a new instance of the color manager class which takes in an original image, greyscale_image of the original image, greybreak_start value, colors_start
 color_manager = ColorManager("Color_Controls", original_image, grayscale_image, greybreak_start, colors_start)    
 
-
+#variable that represents if we want to save the image 
 save_stuff = False
 
+#variables that hold atributes of the color manager class. They are assigned if we hit the s key to save the image. This is because
+#we have to close the cv2 windows when we ask the user for input and we won't have a reference to the windows or trackbars once the windows are gone
 customized_image = None
 original_image = None
 grayscale_image = None
 colors = None
+
+#variable to represent what key was pressed
 keypressed = cv2.waitKey(30)
 while keypressed != 27 and keypressed != ord('s'):
+    #listen for a keypress every 30 milliseconds
     keypressed = cv2.waitKey(30)
+    #if we hit the esc key, destroy all the windows
     if keypressed == 27:
         cv2.destroyAllWindows()
+    #if we hit the save key, set the save_stuff variable to true so we are prompted to enter in names to save the image and colro data
+    #we also assign the variables that hold a reference to all the data that we might want to save such as the final image and the color values
     elif keypressed == ord('s'): 
         save_stuff = True
         customized_image = color_manager.get_customized_image()
@@ -99,13 +112,19 @@ while keypressed != 27 and keypressed != ord('s'):
         colors = color_manager.get_state()
         cv2.destroyAllWindows()
 
+#check if we want to save stuff
 if save_stuff:
+    #ask the user to enter a file name for the image
     file_name = input('Save Customized Image as:')
+    #save the original, gray, and final image with the name the user gave
     save_image(file_name, customized_image)
     save_image(file_name, grayscale_image, '_grayscale')
     save_image(file_name, original_image, '_original')
+    #prompt the user to enter a path to enter in colors
     colorPath = input("Save color file as:")
+    #spilt the file name the user provided from the extension extension 
     root, ext = os.path.splitext(colorPath)
+    #save the file with the file name the user entered and add a .json file extension to it
     with open(root+".json", 'w') as f:
         json.dump(colors,f)
         cv2.destroyAllWindows()
