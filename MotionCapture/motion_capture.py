7dd3a6e8098518da29b0dd3b9b5cf41cb765aaf5
Siddhu M <61168189+RedRangerr@users.noreply.github.com>
2022-11-02 14:55:07 -0500

    sss

--- MotionCapture/motion_capture.py
+++ MotionCapture/motion_capture.py
@@ -13,26 +13,51 @@ def mouseClick(event, x, y, flags, params):
 
 cap = cv2.VideoCapture(0) #0 is webcam number
 #104,104,185
-c1_upper = [4, 180, 200]
-c1_lower = [0, 100, 120]
-c1_upper = np.array(c1_upper, dtype='uint8')
-c1_lower = np.array(c1_lower, dtype='uint8')
+blue_upper = [107, 255, 200]
+blue_lower = [99, 117, 50]
 
 
+#blue azores
+blue_upper = np.array(blue_upper, dtype='uint8')
+blue_lower = np.array(blue_lower, dtype='uint8')
+#diva glam
+pink_upper = np.array([177, 200, 180 ], dtype='uint8')
+pink_lower = np.array([167, 100, 120], dtype='uint8')
+#Violet Obsession
+purple_upper = np.array([137, 100, 130 ], dtype='uint8')
+purple_lower = np.array([124, 25, 80 ], dtype='uint8')
 
 cv2.namedWindow("Webcam")
 cv2.namedWindow("Filter")
 cv2.setMouseCallback("Webcam", mouseClick, param=None)
+kernel = np.ones((5,5), np.uint8)
 while True:
     ret, frame = cap.read()
 
     #convert image to hsv
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(frame_hsv,c1_lower, c1_upper)
-    c1_filter = cv2.bitwise_or(frame, frame, mask = mask)
+    #create blue mask
+    blue_mask = cv2.inRange(frame_hsv,blue_lower, blue_upper)
+    blue_mask = cv2.erode(blue_mask, kernel, iterations=1)
+    blue_filter = cv2.bitwise_or(frame, frame, mask = blue_mask)
+
+    #create pink mask
+    pink_mask = cv2.inRange(frame_hsv, pink_lower, pink_upper)
+    pink_mask = cv2.erode(pink_mask, kernel, iterations=1)
+    pink_filter = cv2.bitwise_or(frame, frame, mask=pink_mask)
+
+    #create purple filter
+    purple_mask = cv2.inRange(frame_hsv, purple_lower, purple_upper)
+    purple_mask = cv2.erode(purple_mask, kernel, iterations=1)
+    purple_filter = cv2.bitwise_or(frame, frame, mask=purple_mask)
+
+    #create filtered image
+    filtered_image = cv2.bitwise_or(blue_filter, pink_filter)
+    filtered_image = cv2.bitwise_or(filtered_image, purple_filter)
+
     cv2.imshow("Webcam", frame)
-    cv2.imshow("Filter", c1_filter)
+    cv2.imshow("Filter", filtered_image)
 
     if cv2.waitKey(1) == ord('q'):
         break
